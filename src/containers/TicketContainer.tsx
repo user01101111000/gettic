07836d542245f80b76921a779bdfb34d732d27b4
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2025-03-10 19:47:56 +0400

    small changes

--- src/containers/TicketContainer.tsx
+++ src/containers/TicketContainer.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Title from "../components/Containers/TicketContainer/Title";
 import UserTicket from "../components/Containers/TicketContainer/UserTicket";
+import LoadingPage from "../components/Layout/LoadingPage";
 
 const TicketContainer: () => React.JSX.Element = (): React.JSX.Element => {
 
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+    React.useLayoutEffect((): void => {
+        setIsLoading(true);
+        setTimeout((): void => {
+            setIsLoading(false);
+        }, 700);
+    }, []);
+
     return <section className="flex flex-col items-center justify-center p-2 gap-5 lg:p-4 lg:gap-7">
+
+        {isLoading && <LoadingPage />}
 
         <Title />
         <UserTicket />
