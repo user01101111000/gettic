e3c205ab2ac0d53dcdf835bcaf7ccc23d970a6f4
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2025-03-10 18:44:33 +0400

    small changes

--- src/pages/Ticket.tsx
+++ src/pages/Ticket.tsx
@@ -8,34 +8,34 @@ import useUser from "../hooks/useUser";
 
 const Ticket: () => React.JSX.Element = (): React.JSX.Element => {
 
-    const [isLoading, setLoading] = React.useState<boolean>(false);
+    const [isLoading, setLoading] = React.useState<boolean>(true);
     const [isError, setIsError] = React.useState<boolean>(false);
 
     const { user } = useUser()!;
     const { id } = useParams();
 
-    // React.useLayoutEffect((): void => {
+    React.useLayoutEffect((): void => {
 
-    //     const template_params = {
-    //         "from_name": "Coding Conference",
-    //         "full_name": user?.full_name ?? "Unknown",
-    //         "to_email": user?.email ?? "unknown@gmail",
-    //         "github_username": user?.github_username ?? "unknown",
-    //         "ticket_id": "#" + id?.slice(0, 5)
-    //     };
+        const template_params = {
+            "from_name": "Gettic",
+            "full_name": user?.full_name ?? "Unknown",
+            "to_email": user?.email ?? "unknown@gmail",
+            "github_username": user?.github_username ?? "unknown",
+            "ticket_id": "#" + id?.slice(0, 5)
+        };
 
-    //     emailjs
-    //         .send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, template_params, {
-    //             publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-    //         })
-    //         .catch((error): void => {
-    //             console.log('FAILED...', error);
-    //             setIsError(true);
-    //         })
-    //         .finally((): void => setLoading(false));
+        emailjs
+            .send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, template_params, {
+                publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+            })
+            .catch((error): void => {
+                console.log('FAILED...', error);
+                setIsError(true);
+            })
+            .finally((): void => setLoading(false));
 
 
-    // }, []);
+    }, []);
 
 
     return <>
