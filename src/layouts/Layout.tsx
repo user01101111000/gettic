5a5c0d8c6c9366ad4b1d3e5291364b3a249dc12d
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2025-03-11 18:00:33 +0400

    smaill ui update

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,13 +5,15 @@ import LoadingPage from "../components/Layout/LoadingPage";
 
 const Layout: () => React.JSX.Element = (): React.JSX.Element => {
 
-    return <main className="min-h-dvh w-full grid grid-rows-[auto_1fr] text-white relative overflow-hidden">
+    return <main className="min-h-dvh w-full grid grid-rows-[auto_1fr] text-white relative">
 
-        <img height={"20px"} width={"20px"} className="absolute inset-0 object-cover object-center w-full h-full -z-50 aspect-square" src="/images/background-desktop.webp" alt="bg" />
-        <img height={"20px"} width={"20px"} className="absolute top-0 left-0 w-full h-full object-cover" src={"/images/pattern-lines.svg"} alt="lines" />
-        <img height={"20px"} width={"20px"} className="absolute top-10 right-0 w-80" src={"/images/pattern-squiggly-line-top.svg"} alt="squiggly-line-top" />
-        <img height={"20px"} width={"20px"} className="absolute bottom-0 left-0 w-[1000px]" src={"/images/pattern-squiggly-line-bottom-desktop.svg"} alt="squiggly-line-bottom" />
-        <img height={"20px"} width={"20px"} className="absolute top-1/2 right-1/12 w-40" src="/images/pattern-circle.svg" alt="circle" />
+        <figure className="absolute inset-0 w-full h-full overflow-hidden -z-50">
+            <img height={"20px"} width={"20px"} className="absolute inset-0 object-cover object-center w-full h-full aspect-square" src="/images/background-desktop.webp" alt="bg" />
+            <img height={"20px"} width={"20px"} className="absolute top-0 left-0 w-full h-full object-cover" src={"/images/pattern-lines.svg"} alt="lines" />
+            <img height={"20px"} width={"20px"} className="absolute top-10 right-0 w-80" src={"/images/pattern-squiggly-line-top.svg"} alt="squiggly-line-top" />
+            <img height={"20px"} width={"20px"} className="absolute bottom-0 left-0 w-[1000px]" src={"/images/pattern-squiggly-line-bottom-desktop.svg"} alt="squiggly-line-bottom" />
+            <img height={"20px"} width={"20px"} className="absolute top-1/2 right-1/12 w-40" src="/images/pattern-circle.svg" alt="circle" />
+        </figure>
 
         <Header />
 
